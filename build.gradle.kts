e1b1cefffcea10686115c5cf1a553ca701a390ba
jp <jp@nowhere.com>
2023-05-23 15:32:25 -0400

    SETUP jpackageplugin win exe
    SETUP jpackageplugin win exe
    
    SETUP jpackageplugin win exe
    
    SETUP jpackageplugin win exe
    
    SETUP jpackageplugin win exe
    
    SETUP jpackageplugin win exe
    
    SETUP jpackageplugin win exe
    
    SETUP jpackageplugin win exe
    
    SETUP jpackageplugin win exe

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-plugins {
-    id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-}
+
 group = "edu.ucla.belief"
 version = "4.0"
--- samiam/build.gradle.kts
+++ samiam/build.gradle.kts
@@ -1,10 +1,11 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import org.panteleyev.jpackage.ImageType
+import org.panteleyev.jpackage.JPackageTask
 
 plugins {
     id("java")
     id("application")
-    id("com.github.johnrengelman.shadow")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
     id("org.panteleyev.jpackageplugin") version "1.5.2"
 
 }
@@ -16,29 +17,76 @@ tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
 }
 group = parent!!.group
 version = parent!!.version
-tasks.jpackage {
-    dependsOn("shadowJar")
+val shadowJarName = "${project.name}-${version}-all.jar"
 
-    input = "build/libs"
+println("${parent!!.version} parent!!.version")
+println("${version} version")
+
+tasks.javadoc {
+
+}
+
+task("packageNetworkSamples", Copy::class) {
+    group = "distribution"
+    from("${rootDir}/network_samples").into("$buildDir/package/network_samples")
+}
+task("packageShadowJar", Copy::class) {
+    group = "distribution"
+    dependsOn("shadowJar")
+    from("${buildDir}/libs/$shadowJarName").into("$buildDir/package")
+}
+
+tasks.jpackage {
+    dependsOn("packageNetworkSamples", "packageShadowJar")
+    group = "distribution"
+
+    input = "build/package"
     destination = "$buildDir/dist"
 
-    appName = "samiam-$version"
+    appName = project.name
     vendor = "ucla"
 
-    mainJar = "${project.name}-${project.version}-all.jar"
+    mainJar = shadowJarName
     mainClass = main
+    icon = "${projectDir}/src/main/resources/SamIamAppIcon.png"
 
     javaOptions = listOf("-Dfile.encoding=UTF-8")
-
     windows {
-        winConsole = true
+        type = ImageType.MSI
+        winMenu = true
+        winDirChooser = true
+        appName = project.name
+        winShortcutPrompt = true
+        winMenuGroup = "SamIam"
+        icon = "${projectDir}/src/main/resources/samiamappicon_c9a_icon.ico"
+
     }
     linux {
-        appName = "samiam-$version.AppImage"
-        type = ImageType.APP_IMAGE
-        winConsole = true
+        type = ImageType.DEFAULT
+        linuxShortcut = true
+        linuxAppCategory = "SamIam"
+    }
+    mac {
+        type = ImageType.DMG
     }
 }
+
+tasks.create("jpackageAppImage", JPackageTask::class) {
+    group = "distribution"
+    dependsOn("shadowJar")
+    input = "build/libs"
+    destination = "$buildDir/dist"
+    appName = "samiam-$version"
+    vendor = "ucla"
+    mainJar = shadowJarName
+    mainClass = main
+    javaOptions = listOf("-Dfile.encoding=UTF-8")
+    linux {
+        appName = "${project.name}-$version.AppImage"
+        type = ImageType.APP_IMAGE
+    }
+}
+
 repositories {
     mavenCentral()
 }
